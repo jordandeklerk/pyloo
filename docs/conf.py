c5c764f7c5bc599c4e0b8877c08204313fe094f1
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-01-29 17:54:24 -0500

    Add config files for development repo setup

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+project = 'pyloo'
+copyright = '2025, Jordan Deklerk'
+author = 'Jordan Deklerk'
+
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_type_aliases = None
---
+++ src/pyloo/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyloo: Python implementation of Leave-One-Out cross-validation
+
+This package provides tools for computing and working with Leave-One-Out (LOO) 
+cross-validation, particularly focused on Bayesian models and their variants.
+"""
+
+__version__ = "0.1.0"
