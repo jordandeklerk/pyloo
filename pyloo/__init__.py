8dfea7dc940b83c40fe5c1b85d9aa12511c8c3eb
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-02-06 23:25:16 -0500

    add init

--- pyloo/__init__.py
+++ pyloo/__init__.py
@@ -1,3 +1,10 @@
-"""Python implementation of Pareto Smoothed Importance Sampling Leave-One-Out Cross-Validation."""
+"""Leave-one-out cross-validation (LOO-CV) using importance sampling methods."""
+
+from .diagnostics import ParetokTable
+from .elpd import ELPDData
+from .loo import loo
+from .psis import psislw
+
+__all__ = ["loo", "psislw", "ELPDData", "ParetokTable"]
 
 __version__ = "0.1.0"
