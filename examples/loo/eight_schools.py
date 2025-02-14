47ed7786fb0670fb7096390860545fcd7597384b
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-02-13 21:57:14 -0500

    add real model comparison example

--- examples/loo/eight_schools.py
+++ examples/loo/eight_schools.py
@@ -10,6 +10,7 @@ import pymc as pm
 project_root = str(Path(__file__).parent.parent.parent)
 sys.path.append(project_root)
 
+from pyloo.compare import loo_compare  # noqa: E402
 from pyloo.loo import loo  # noqa: E402
 from pyloo.loo_subsample import loo_subsample  # noqa: E402
 
@@ -37,7 +38,23 @@ with pm.Model() as eight_schools:
         random_seed=42,
         return_inferencedata=True,
         idata_kwargs={"log_likelihood": True},
-        target_accept=0.95,
+    )
+
+
+with pm.Model() as eight_schools_2:
+    mu = pm.Normal("mu", mu=0, sigma=10)
+    tau = pm.InverseGamma("tau", alpha=2, beta=5)
+
+    theta = pm.Normal("theta", mu=mu, sigma=tau, shape=schools)
+    y = pm.Normal("y", mu=theta, sigma=treatment_stddevs, observed=treatment_effects)
+
+    idata_2 = pm.sample(
+        draws=1000,
+        tune=1000,
+        chains=4,
+        random_seed=42,
+        return_inferencedata=True,
+        idata_kwargs={"log_likelihood": True},
     )
 
 logger.info("\nComputing full LOO-CV...")
@@ -45,8 +62,25 @@ loo_results = loo(idata, pointwise=True)
 logger.info(loo_results)
 
 logger.info("\nComputing subsampled LOO-CV...")
-loo_subsample_results = loo_subsample(idata, observations=4, loo_approximation="plpd", pointwise=True)
+loo_results_2 = loo(idata_2, pointwise=True)
+logger.info(loo_results_2)
+
+logger.info("\nComputing subsampled LOO-CV...")
+loo_subsample_results = loo_subsample(idata, observations=4, pointwise=True)
 logger.info(loo_subsample_results)
+
+logger.info("\nComputing model comparison...")
+comparison_results = loo_compare({"model1": loo_results, "model2": loo_results_2})
+logger.info(comparison_results)
+
+logger.info("\nComputing model comparison with Bayesian bootstrap...")
+comparison_results_bb = loo_compare(
+    {"model1": loo_results, "model2": loo_results_2},
+    method="bb-pseudo-bma",
+    alpha=0.3,
+    b_samples=1000,
+)
+logger.info(comparison_results_bb)
 
 logger.info("\nComparison of estimates:")
 logger.info(f"Full LOO ELPD:      {loo_results.elpd_loo:.2f} ± {loo_results.se:.2f}")
